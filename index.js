b88c7730aeec3003137a6503d8b6772e16954f5d
andrewlowell <andrewlowellr@gmail.com>
2019-04-02 10:53:14 -0600

    Finish MVP.

--- index.js
+++ index.js
@@ -7,10 +7,34 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const authenticated = (req, res, next) => {
+  const { username, password } = req.headers;
+  console.log(username, password)
+  if (username && password) {
+    db('users')
+      .where({ username: username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        }
+        else {
+          res.status(401).json({ message: "Credentials are invalid." })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Something went wrong" })
+      });
+  }
+  else {
+    res.status(400).json({ message: "Please provide a username and password" })
+  }
+}
+
 server.post('/api/register', (req, res) => {
   const { username, password } = req.body;
   const hash = bcrypt.hashSync(password, 4);
-  db.insert({
+  db('users').insert({
     username: username,
     password: hash
   })
@@ -20,4 +44,36 @@ server.post('/api/register', (req, res) => {
   .catch(error => {
     res.status(400).json({ message: "Register unsuccessful"});
   })
+});
+
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  db('users')
+  .where({ username: username })
+  .first()
+  .then(user => {
+    console.log('user login', user);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ message: "Successfully logged in!" })
+    }
+    else {
+      res.status(401).json({ message: "Invalid credentials." })
+    }
+  })
+  .catch(err => {
+    res.status(400).json({ message: "You shall not pass" })
+  })
+});
+
+server.get('/api/users', authenticated, (req, res) => {
+  db('users')
+  .then(users => {
+    res.json(users);
+  })
+  .catch(err => {
+    res.status(500).json({ message: "Something went wrong" });
+  })
 })
+
+const port = process.env.PORT || 5555;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
